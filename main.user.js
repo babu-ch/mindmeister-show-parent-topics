483e2ea95a66838ff2440444735c14f4b76e1aa7
babu-ch <mashirohira@gmail.com>
2022-03-16 14:48:21 +0900

    rename

---
+++ main.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         mindmister show-parent-topics
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       babu-ch
+// @match        https://www.mindmeister.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mindmeister.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const infoEl = document.createElement('div')
+    infoEl.id = 'infoEl'
+    infoEl.style.position = 'fixed'
+    infoEl.style.top = 0
+    infoEl.style.left = 0
+    infoEl.style['z-index'] = 999
+    infoEl.style.background = 'white'
+    document.body.append(infoEl)
+
+    function getParentTopic(el) {
+        const wrap = el.closest('.tk_children')
+        if (!wrap) {
+            return
+        }
+        const id = wrap.id.replace('tk_children_', '')
+       const parentTopic = document.getElementById(id)
+       if (!parentTopic) {
+           return null
+       }
+        return parentTopic.classList.contains('root_child') ? null : parentTopic
+    }
+    document.addEventListener('click', function(e) {
+        const clicked = e.target
+        infoEl.innerText = ''
+        if (!clicked.classList.contains('tk_title')) {
+            return
+        }
+
+        const topicTexts = []
+        let topic = clicked
+        while( topic = getParentTopic(topic)) {
+            topicTexts.unshift(topic.innerText)
+        }
+        infoEl.innerText = topicTexts.join('>')
+    })
+})();
